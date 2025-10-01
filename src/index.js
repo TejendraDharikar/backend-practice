37898a0035f6312201bd1ea7b6215cb6516832f3
TejendraDharikar <tejendradharikar@gmail.com>
2025-10-02 01:17:14 +0545

    utils code

--- src/index.js
+++ src/index.js
@@ -7,9 +7,16 @@ dotenv.config({
   });
 
 
-connectDB();
-
-
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT||8000,()=>{
+    console.log(`server is running at port:${process.env.PORT||8000}`);
+  })
+})
+.catch((error) => {
+  console.error("mongodb connection failed!!!", error);
+  throw error;
+})
 
 
 
